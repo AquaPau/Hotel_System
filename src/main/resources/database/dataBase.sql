2264ef49aa87426a05956ad371278dcf0d8ef742
Polina Semenova <polina_semenova@epam.com>
2019-02-05 16:07:01 +0300

    final ver of DB - hotfix rerun script for existing DB and admin role

--- src/main/resources/database/dataBase.sql
+++ src/main/resources/database/dataBase.sql
@@ -1,11 +1,11 @@
-CREATE DATABASE Hotel;
+SELECT exists(SELECT datname FROM pg_catalog.pg_database where datname='Hotel');
 CREATE SCHEMA IF NOT EXISTS hotel;
-DROP TYPE IF EXISTS hotel.permission CASCADE ;
+DROP TYPE IF EXISTS hotel.permission CASCADE;
 CREATE TYPE hotel.permission AS ENUM ('ADMIN', 'USER');
 DROP TYPE IF EXISTS hotel.classID CASCADE ;
-CREATE TYPE hotel.classID AS ENUM ('ECONOM', 'STANDARD', 'FAMILY', 'LUXE');
+CREATE TYPE hotel.classID AS ENUM ('ECONOMY', 'STANDARD', 'FAMILY', 'LUX');
 DROP TYPE IF EXISTS hotel.capacity CASCADE ;
-CREATE TYPE hotel.capacity AS ENUM ('1', '2', '3', '4');
+CREATE TYPE hotel.capacity AS ENUM ('SINGLE', 'DOUBLE', 'TRIPLE', 'QUAD');
 DROP TYPE IF EXISTS hotel.paymentStatus CASCADE ;
 CREATE TYPE hotel.paymentStatus AS ENUM ('PAID', 'BILLSENT', 'NOBILL');
 
@@ -24,7 +24,7 @@ CREATE TABLE IF NOT EXISTS hotel.Rooms
 (
   roomNumber SMALLINT PRIMARY KEY,
   classID    hotel.classID  default 'STANDARD',
-  capacity   hotel.capacity default '1',
+  capacity   hotel.capacity default 'SINGLE',
   price      MONEY NOT NULL
 );
 
@@ -32,7 +32,7 @@ CREATE TABLE IF NOT EXISTS hotel.Requests
 (
   requestID     UUID PRIMARY KEY,
   userID        UUID        NOT NULL REFERENCES hotel.Users (userID),
-  capacity      hotel.capacity    default '1',
+  capacity      hotel.capacity    default 'SINGLE',
   classID       hotel.classID     default 'STANDARD',
   checkIn       TIMESTAMPTZ NOT NULL,
   checkOut      TIMESTAMPTZ NOT NULL,
@@ -45,6 +45,13 @@ CREATE TABLE IF NOT EXISTS hotel.ReservedRooms
   requestID UUID REFERENCES hotel.Requests
 );
 
-CREATE USER ADMIN WITH PASSWORD 'ADMIN';
+DO $$
+  BEGIN
+    IF NOT EXISTS (SELECT 1 FROM pg_roles WHERE rolname = 'admin') THEN
+      CREATE ROLE admin LOGIN PASSWORD 'admin';
+    END IF;
+  END
+  $$;
+
 GRANT ALL PRIVILEGES ON DATABASE Hotel TO ADMIN;
 
