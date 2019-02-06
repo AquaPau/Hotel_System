564c95712f640cc9141f046e5952ade2aa78822c
Ilya Dudkin <ilya_dudkin@epam.com>
2019-02-06 14:48:07 +0300

    fixed userId type to long

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -7,7 +7,7 @@ import lombok.Data;
 @Data
 public class User {
 
-    private int id;
+    private long id;
     private String login;
     private Permission permission;
     private String firstName;
