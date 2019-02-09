b612c30986f7c17aa5c061678c371c277e4c1cf5
Alexandr Davydov <davidoff_92@inbox.ru>
2019-02-10 00:39:09 +0300

    thymeleaf support and elementary user registration

--- src/main/java/com/epam/hotel/controllers/UserController.java
+++ src/main/java/com/epam/hotel/controllers/UserController.java
@@ -1,21 +1,43 @@
 package com.epam.hotel.controllers;
 
+import com.epam.hotel.model.User;
 import com.epam.hotel.services.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class UserController {
     private final UserService userService;
 
-    @GetMapping("/")
-    public String getUsers(Model model) {
-        model.addAttribute("userList", userService.getAll());
+    @GetMapping({"/", "index"})
+    public String getIndex(Model model) {
+        //model.addAttribute("userList", userService.getAll());
         return "index";
     }
+
+    @GetMapping("/login")
+    public String getUsers(Model model) {
+        model.addAttribute("user", new User());
+        return "login";
+    }
+
+    @GetMapping("/register")
+    public String showRegistrationForm(Model model) {
+        model.addAttribute("user", new User());
+        return "register";
+    }
+
+    @PostMapping("/register")
+    public String saveOrUpdate(@ModelAttribute User user) {
+        userService.create(user);
+        return "redirect:index";
+    }
+
 
 }
