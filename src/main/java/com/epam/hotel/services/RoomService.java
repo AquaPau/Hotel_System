65565ffc8ab2127c44cd2cced5f741523cfc8a2c
Polina Semenova <polina_semenova@epam.com>
2019-02-14 00:35:46 +0300

    Created a rooms page but hasn't resolved yet the problem of view the page. Can't add logical buttons from interfaces to the pages (wait for merge from Sasha's branch

--- src/main/java/com/epam/hotel/services/RoomService.java
+++ src/main/java/com/epam/hotel/services/RoomService.java
@@ -1,9 +1,14 @@
 package com.epam.hotel.services;
 
+import com.epam.hotel.dtos.RoomDto;
 import com.epam.hotel.model.Room;
 
 public interface RoomService extends Service<Room> {
 
     public Room getByRoomNumber(int roomNumber);
+
+    RoomDto getRoomDto(Room room);
+
+    Room getRoomFromDto(RoomDto roomDto);
 
 }
