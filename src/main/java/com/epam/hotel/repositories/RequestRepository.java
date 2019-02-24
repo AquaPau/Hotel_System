66fc210880ee49c8ae757b47b15f20d204c8f1fd
Alexandr Davydov <davidoff_92@inbox.ru>
2019-02-24 17:19:03 +0300

    added index page

--- src/main/java/com/epam/hotel/repositories/RequestRepository.java
+++ src/main/java/com/epam/hotel/repositories/RequestRepository.java
@@ -1,7 +1,16 @@
 package com.epam.hotel.repositories;
 
 import com.epam.hotel.model.Request;
+import com.epam.hotel.model.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RequestRepository extends JpaRepository<Request, Long> {
+
+    Page<Request> findAllByUserAndRoomNull(User user, Pageable pageable);
+
+    Page<Request> findAllByUserAndRoomNotNull(User user, Pageable pageable);
+
+
 }
--- src/main/java/com/epam/hotel/repositories/ReservedRoomRepository.java
+++
@@ -1,7 +0,0 @@
-package com.epam.hotel.repositories;
-
-import com.epam.hotel.model.ReservedRoom;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReservedRoomRepository extends JpaRepository<ReservedRoom, Long> {
-}
