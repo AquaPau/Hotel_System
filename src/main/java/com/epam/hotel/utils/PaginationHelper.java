f19ab193994dc1dfcd04ba59a102e510be36df40
Ilya Dudkin <ilya_dudkin@epam.com>
2019-02-27 19:26:54 +0300

    Fixed code duplication on Admin Controller

--- src/main/java/com/epam/hotel/utils/PaginationHelper.java
+++ src/main/java/com/epam/hotel/utils/PaginationHelper.java
@@ -10,6 +10,16 @@ public class PaginationHelper {
 
     private PaginationHelper() {}
 
+    public static Integer getPage(Integer page) {
+        if (page == null || page < 1) page = 1;
+        return page;
+    }
+
+    public static Integer getLimit(Integer limit, Integer defaultLimit) {
+        if (limit == null || limit < 1) limit = defaultLimit;
+        return limit;
+    }
+
     public static List<Integer> getPageNumber(Page<? extends Object> pagedList) {
         List<Integer> pageNumbers = IntStream.rangeClosed(1, pagedList.getTotalPages()).
                 boxed().collect(Collectors.toList());
