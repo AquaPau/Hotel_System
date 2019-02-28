645cefac08bec254df03d45d25fe7c0b3b0ea876
Aleksandr Davydov <aleksandr_davydov2@epam.com>
2019-02-28 17:27:33 +0300

    room repository and request service clean

--- src/main/java/com/epam/hotel/repositories/RoomRepository.java
+++ src/main/java/com/epam/hotel/repositories/RoomRepository.java
@@ -1,13 +1,8 @@
 package com.epam.hotel.repositories;
 
 import com.epam.hotel.domains.Room;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
 
 public interface RoomRepository extends JpaRepository<Room, Long> {
-
-    Page<Room> findAllByIdIsNotIn(List<Room> notAvailableRooms, Pageable pageable);
 
 }
