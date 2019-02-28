9f92ec448d72a136a45bb86e73f125a4c300a096
Ilya Dudkin <ilya_dudkin@epam.com>
2019-02-28 19:18:25 +0300

    Created tests for DenyMessageService, RoomService

--- src/test/java/com/epam/hotel/HotelApplicationTests.java
+++ src/test/java/com/epam/hotel/HotelApplicationTests.java
@@ -2,15 +2,28 @@ package com.epam.hotel;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@WebMvcTest
 public class HotelApplicationTests {
 
+	@Autowired
+	MockMvc mockMvc;
+
 	@Test
-	public void contextLoads() {
+	public void contextLoads() throws Exception {
+		MvcResult mvcResult = mockMvc.perform(
+				MockMvcRequestBuilders.get("/all/").accept(MediaType.APPLICATION_JSON)
+		).andReturn();
 	}
 
 }
