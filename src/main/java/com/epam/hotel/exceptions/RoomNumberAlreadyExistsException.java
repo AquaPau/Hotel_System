5cd55c24ad93de22ee9a9d02b7d485f9bf32d924
Polina <36632855+AquaPau@users.noreply.github.com>
2019-03-08 23:09:36 +0300

    Delete RoomNumberAlreadyExistsException.java

--- src/main/java/com/epam/hotel/exceptions/RoomNumberAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package com.epam.hotel.exceptions;
-
-public class RoomNumberAlreadyExistsException extends RuntimeException {
-    public RoomNumberAlreadyExistsException(String message){
-        super(message);
-    }
-}
