8cd30ea08ef18b0cde2a4a913ce8a45b94b969cb
Polina <36632855+AquaPau@users.noreply.github.com>
2019-03-08 23:09:31 +0300

    Delete PasswordDoesNotMatchException.java

--- src/main/java/com/epam/hotel/exceptions/PasswordDoesNotMatchException.java
+++
@@ -1,8 +0,0 @@
-package com.epam.hotel.exceptions;
-
-public class PasswordDoesNotMatchException extends RuntimeException {
-
-    public PasswordDoesNotMatchException(String message) {
-        super(message);
-    }
-}
