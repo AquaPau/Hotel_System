787996c8fea8a783706401b4d876c6652b3f84a7
Polina <36632855+AquaPau@users.noreply.github.com>
2019-03-08 23:09:45 +0300

    Delete DenyMessageRepository.java

--- src/main/java/com/epam/hotel/repositories/DenyMessageRepository.java
+++
@@ -1,8 +0,0 @@
-package com.epam.hotel.repositories;
-
-import com.epam.hotel.domains.DeniedRequest;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DenyMessageRepository extends JpaRepository<DeniedRequest, Long> {
-
-}
